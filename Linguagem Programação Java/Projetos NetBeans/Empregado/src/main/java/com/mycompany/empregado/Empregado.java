975716c7f3a628c32e91929514e2a4e1476b39fc
João Raposo <joao.d.t.raposo@upskill.pt>
2021-01-05 09:24:13 +0000

    Criação de Utilitarios e Empregado || Criação de Classes Tempo&Data&Empregado & adição de dependencias

---
+++ Linguagem Programação Java/Projetos NetBeans/Empregado/src/main/java/com/mycompany/empregado/Empregado.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.empregado;
+
+import com.mycompany.utilitarios.*;
+
+
+/**
+ *
+ * @author joaor
+ */
+public class Empregado  {
+    
+    private String primeiroNome;
+    private String ultimoNome;
+    private Data dataContrato;
+    private Tempo horaEntrada;
+    private Tempo horaSaida;
+    
+    public Empregado(String primeiroNome, String ultimoNome, Data dataContrato, Tempo horaEntrada, Tempo horaSaida){
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.dataContrato = new Data(dataContrato);
+        this.horaEntrada = new Tempo(horaEntrada);
+        this.horaSaida = new Tempo(horaSaida);
+    }
+
+    /**
+     * @return the primeiroNome
+     */
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    /**
+     * @param primeiroNome the primeiroNome to set
+     */
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    /**
+     * @return the ultimoNome
+     */
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+    /**
+     * @param ultimoNome the ultimoNome to set
+     */
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    /**
+     * @return the dataContrato
+     */
+    public Data getDataContrato() {
+        return dataContrato;
+    }
+
+    /**
+     * @param dataContrato the dataContrato to set
+     */
+    public void setDataContrato(Data dataContrato) {
+        this.dataContrato = dataContrato;
+    }
+
+    /**
+     * @return the horaEntrada
+     */
+    public Tempo getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    /**
+     * @param horaEntrada the horaEntrada to set
+     */
+    public void setHoraEntrada(Tempo horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    /**
+     * @return the horaSaida
+     */
+    public Tempo getHoraSaida() {
+        return horaSaida;
+    }
+
+    /**
+     * @param horaSaida the horaSaida to set
+     */
+    public void setHoraSaida(Tempo horaSaida) {
+        this.horaSaida = horaSaida;
+    }
+    
+    
+    
+}
