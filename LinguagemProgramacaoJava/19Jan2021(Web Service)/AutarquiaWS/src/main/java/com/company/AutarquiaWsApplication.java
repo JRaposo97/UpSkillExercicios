e207ac9da1736e09d3cc74321e7d832f24ba8c33
João Raposo <joao.d.t.raposo@upskill.pt>
2021-01-19 18:03:52 +0000

    Updates

--- LinguagemProgramacaoJava/19Jan2021(Web Service)/AutarquiaWS/src/main/java/com/company/AutarquiaWsApplication.java
+++ LinguagemProgramacaoJava/19Jan2021(Web Service)/AutarquiaWS/src/main/java/com/company/AutarquiaWsApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutarquiaWsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AutarquiaWsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.getProperties().put("server.port", 8181);
+        SpringApplication.run(AutarquiaWsApplication.class, args);
+    }
 
 }
