e728f44205d0660f413564dcd54a11d072a99d0e
João Raposo <joao.d.t.raposo@upskill.pt>
2021-01-20 10:57:10 +0000

    Controller + service

--- LinguagemProgramacaoJava/19Jan2021(Web Service)/AutarquiaWS/src/main/java/com/company/service/FreguesiasService.java
+++ LinguagemProgramacaoJava/19Jan2021(Web Service)/AutarquiaWS/src/main/java/com/company/service/FreguesiasService.java
@@ -5,10 +5,67 @@
  */
 package com.company.service;
 
+import com.company.dto.ListaFreguesiaDTO;
+import com.company.dto.Mapper;
+import com.company.exception.ConversaoException;
+import com.company.model.Autarquia;
+import com.company.model.Freguesia;
+import com.company.repo.Dados;
+import java.util.ArrayList;
+
 /**
  *
  * @author joaor
  */
 public class FreguesiasService {
 
+    public static ListaFreguesiaDTO getFreguesias() {
+        ListaFreguesiaDTO listaFreguesiaDTO = null;
+        Autarquia autarquia = Dados.carregarDados();
+        ArrayList<Freguesia> freguesias = autarquia.getFreguesias();
+        listaFreguesiaDTO = Mapper.listPessoa2PessoaDTO(pessoas);
+        return listaPessoaDTO;
+    }
+
+    public static PessoaDTO getPessoa(long nif) {
+        Autarquia autarquia = Dados.carregarDados();
+        Pessoa pessoa = autarquia.getPessoa(nif);
+        if (pessoa == null) {
+            return null;
+        }
+        PessoaDTO pessoaDTO = Mapper.pessoa2PessoaDTO(pessoa);
+        if (pessoaDTO != null) {
+            return pessoaDTO;
+        } else {
+            throw new ConversaoException("PessoaDTO");
+        }
+    }
+
+    public static void addPessoa(PessoaDTO pessoaDTO) {
+        Pessoa pessoa = Mapper.pessoaDTO2Pessoa(pessoaDTO);
+        if (pessoa != null) {
+            Autarquia autarquia = Dados.carregarDados();
+            autarquia.addPessoa(pessoa);
+            Dados.guardarDados(autarquia);
+        } else {
+            throw new ConversaoException("PessoaDTO");
+        }
+    }
+
+    public static void updatePessoa(long nif, PessoaDTO pessoaDTO) {
+        Pessoa pessoa = Mapper.pessoaDTO2Pessoa(pessoaDTO);
+        if (pessoa != null) {
+            Autarquia autarquia = Dados.carregarDados();
+            autarquia.updatePessoa(nif, pessoa);
+            Dados.guardarDados(autarquia);
+        } else {
+            throw new ConversaoException("PessoaDTO");
+        }
+    }
+
+    public static void removePessoa(long nif) {
+        Autarquia autarquia = Dados.carregarDados();
+        autarquia.removePessoa(nif);
+        Dados.guardarDados(autarquia);
+    }
 }
